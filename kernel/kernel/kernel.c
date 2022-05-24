1a3700949ec66b53e7550c1b76fc72cdadcfd51e
misprit7 <xandernaumenko@gmail.com>
2022-05-23 22:59:34 -0700

    Got paging tentitavely working

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
+#include <stdint.h>
 
 #include <kernel/tty.h>
 
+uint32_t page_directory[1024] __attribute__((aligned(4096)));
+uint32_t first_page_table[1024] __attribute__((aligned(4096)));
+
+extern void loadPageDirectory(uint32_t*);
+extern void enablePaging();
+
 void kernel_main(void) {
+	for(int i = 0; i < 1024; ++i){
+		page_directory[i] = 0x02;
+	}
+	for(int i = 0; i < 1024; ++i){
+		first_page_table[i] = (i * 0x1000) | 3;
+	}
+
+	page_directory[0] = ((unsigned int)first_page_table) | 3;
+
+	loadPageDirectory(page_directory);
+	enablePaging();
+
 	terminal_initialize();
 	printf("Hello, kernel World!\n");
 }
