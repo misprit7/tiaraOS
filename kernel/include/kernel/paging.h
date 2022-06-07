dfd4800e5ce303d9225512f2de9e0d6a8ad350fb
misprit7 <xandernaumenko@gmail.com>
2022-06-07 00:27:32 -0700

    Switched to higher half kernel

--- kernel/include/kernel/paging.h
+++
@@ -1,7 +0,0 @@
-#ifndef PAGINIG_H
-#define PAGINIG_H
-
-void paging_init(void);
-
-#endif
-
--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -2,11 +2,8 @@
 #include <stdint.h>
 
 #include <kernel/tty.h>
-#include <kernel/paging.h>
 
 void kernel_main(void) {
-
-	paging_init();
 	terminal_init();
 	printf("Hello, kernel World!\n");
 }
